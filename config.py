323f546737d0aee552838eec75cd5f9f6ffdfb7b
Shah Ram <ram.coding8@gmail.com>
2025-01-31 15:58:56 +0530

    added commit

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 class Config:
     SECRET_KEY = 'MyBusinessCardApp'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///db.sqlite3'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://mybusinesscardapp_user:P5GgUlAzKN8JNaR3eHsjmQXEOEiRfwOt@dpg-cuea3t1opnds738enjf0-a.singapore-postgres.render.com/mybusinesscardapp'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
